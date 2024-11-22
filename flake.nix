cfa5410938babf64efa6cbeeee411accfb4628ec
nrs <seba@localhost>
2024-11-22 21:14:42 +0000

    adding kitty

--- flake.nix
+++ flake.nix
@@ -1,4 +1,13 @@
 {
+  nixConfig = {
+    substituters = [
+      "https://cache.nixos.org"
+      "https://nix-community.cachix.org"
+    ];
+    trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
