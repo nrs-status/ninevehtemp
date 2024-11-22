4fbb71fe655ec1f7ccb15c778508767d7e19585c
nrs <seba@localhost>
2024-11-22 20:51:28 +0000

    adding kitty

--- temple_artemis_ephesus/kitty/default.nix
+++ temple_artemis_ephesus/kitty/default.nix
@@ -2,7 +2,7 @@
 let 
   kittyBaseConf = builtins.readFile ./kitty.conf;
   confExtension = import ./toIncludeInConf.nix { inherit pkgs; };
-  totalConf = kittyBaseConf ++ confExtension;
+  totalConf = kittyBaseConf + confExtension;
   asNixStoreFile = pkgs.writeText "kitty.conf" totalConf;
 in
 pkgs.writeScriptBin "kittyb" "${pkgs.lib.getExe pkgs.kitty} -c ${asNixStoreFile}"
