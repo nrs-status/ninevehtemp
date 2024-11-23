2aabd6d783567583caf86bb2742bdd766cf334ba
nrs <seba@localhost>
2024-11-23 02:46:17 +0000

    adding kitty

--- temple_artemis_ephesus/kitties/default.nix
+++ temple_artemis_ephesus/kitties/default.nix
@@ -3,6 +3,6 @@ let
   mkKitty = import ./resources/mkKitty.nix { inherit pkgs; };
 in
 {
-  kitty = mkKitty { scriptName = "kittyb"; baseConfPath = ./kitty.conf; themeFileName = "gruvbox_dark.conf"; };
-  kittylight = mkKitty { scriptName = "kittyc"; baseConfPath = ./kitty.conf; themeFileName = "gruvbox_light.conf"; };
+  kitty = mkKitty { scriptName = "kittyb"; baseConfPath = ./kitty.conf; themeFileName = "gruvbox-dark.conf"; };
+  kittylight = mkKitty { scriptName = "kittyc"; baseConfPath = ./kitty.conf; themeFileName = "gruvbox-light.conf"; };
 }
