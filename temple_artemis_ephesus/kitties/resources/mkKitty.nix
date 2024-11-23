4c49ce4e92ad2ae7c15ef46d20028c54d7b2f4e0
nrs <seba@localhost>
2024-11-23 02:48:52 +0000

    adding kitty

--- temple_artemis_ephesus/kitties/resources/mkKitty.nix
+++ temple_artemis_ephesus/kitties/resources/mkKitty.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 { baseConfPath, themeFileName, scriptName }:
 let
-  themeString = "include ${pkgs.kitty-themes}/share/kitty-themes/${themeFileName}";
+  themeString = "include ${pkgs.kitty-themes}/share/kitty-themes/themes/${themeFileName}";
   baseConf = builtins.readFile baseConfPath;
   confToNixStore = pkgs.writeText "kitty.conf" (baseConf + themeString);
 in
