fd48884e832cd0083386380068a156ad7faab9b2
nrs <seba@localhost>
2025-01-17 02:24:23 +0000

    same

---
+++ temple_artemis_ephesus/agda_lsp2/default.nix
@@ -0,0 +1,34 @@
+{ pkgs, ... }:
+pkgs.stdenv.mkDerivation {
+  name = "agda_lsp";
+  version = "0.2.7.0.1.4";
+
+  src = pkgs.fetchzip {
+    url = "https://github.com/agda/agda-language-server/releases/download/v0.2.7.0.1.4/als-Agda-2.7.0.1-ubuntu.zip";
+    hash = "sha256-kR9DSAXrccl5qLbNNVutWW5B9NvMR0vCVdT1onUdOlM=";
+    stripRoot=false;
+  };
+
+  nativeBuildInputs = with pkgs; [
+    autoPatchelfHook
+  ];
+
+  buildInputs = with pkgs; [
+    zlib
+    icu70
+    libtinfo
+    gmp
+  ];
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/bin
+    cp $src/als $out/bin/als
+    chmod +x $out/bin/als
+
+    runHook postInstall
+  '';
+
+
+}
